e91dfbb0faa386e785a725e5f71a1196b2d32892
nisse <nisse.malmstrom@icloud.com>
2025-04-21 21:24:11 +0200

    changed some things

--- tabby/blocks.py
+++ tabby/blocks.py
@@ -1,14 +1,17 @@
-class _MotionBlocks:
+from pygame.examples.aliens import Player
+
+
+class MotionBlocks:
     def move(self, steps: float):
         pass
 
     def turn(self, degrees: float): # clockwise
         pass
 
-    def go_to(self, position: (int, int)):
+    def go_to(self, position):
         pass
 
-    def glide_to(self, seconds: float, position: (int, int)):
+    def glide_to(self, seconds: float, position):
         pass
 
     def point_in_direction(self, degrees: int): #degrees
@@ -36,14 +39,29 @@ class _MotionBlocks:
         pass
 
 
-class _LooksBlocks:
-    def switch_backdrop_to(self, backdrop: str):
+class LooksBlocks:
+    def say(self, text: str, seconds: float = None):
         pass
 
-    def switch_backdrop_to_and_wait(self, backdrop: str):
+    def think(self, text: str, seconds: float = None):
+        pass
+
+    def switch_costume_to(self, costume):
+        pass
+
+    def next_costume(self):
+        pass
+
+    def switch_backdrop_to(self, backdrop: str, wait: bool = False):
         pass
 
     def next_backdrop(self):
+        pass
+
+    def change_size_by(self, amount: int):
+        pass
+
+    def set_size_to(self, amount: int):
         pass
 
     def change_effect_by(self, effect, amount: float):
@@ -60,13 +78,27 @@ class _LooksBlocks:
     def clear_graphic_effects(self):
         pass
 
-
-# Backdrop
-class _BackdropLooksBlocks:
-    def switch_backdrop_to(self, backdrop: str):
+    def show(self):
         pass
 
-    def switch_backdrop_to_and_wait(self, backdrop: str):
+    def hide(self):
+        pass
+
+    def go_to_layer(self, layer, amount: int = None):
+        pass
+
+class SoundBlocks:
+    def play_sound(self, sound, until_done: bool = False):
+        pass
+
+    def stop_all_sounds(self):
+        pass
+
+
+
+# Backdrop
+class BackdropLooksBlocks:
+    def switch_backdrop_to(self, backdrop: str, wait: bool = False):
         pass
 
     def next_backdrop(self):
--- tabby/packager/__init__.py
+++ tabby/packager/__init__.py
@@ -1,1 +1,6 @@
 from .package import package
+
+
+
+with open("._final_product.json", "w") as _f:
+    _f.write("")
