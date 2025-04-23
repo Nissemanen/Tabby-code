03d2024341b4d5c50bb915d332b4b058491d2efe
Nissemanen <99994541+Nissemanen@users.noreply.github.com>
2025-04-23 09:40:05 +0200

    Update sprite.py

--- src/tabby/sprite.py
+++ src/tabby/sprite.py
@@ -1,10 +1,23 @@
-from src.tabby.blocks import _MotionBlocks
+from src.tabby.blocks import MotionBlocks
+import os
 
-class Sprite(_MotionBlocks):
+__all__ = ["Sprite"]
+_final_path = os.path.join(os.path.dirname(__file__), "package\\_final_product.json")
+
+class Sprite(MotionBlocks, LooksBlocks, SoundBlocks, EventBlocks, ControlBlocks):
+    position = (0, 0)
+    size = 100
+    show = True
+    rotation = 90
+    rotation_style = "All_Around"
+    
     def __init__(self, name: str, position: (int, int) = (0, 0), show: bool = True, size: int = 100, rotation: int = 90, rotation_style: str = "All_Around"):
         self.position = position
         self.size = size
         self.show = show
         self.rotation = rotation
         self.rotation_style = rotation_style
+        
+        
 
+    
