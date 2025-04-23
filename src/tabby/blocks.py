e305c1e3db69ca80c3d0a046de81301ce313ba42
Nissemanen <99994541+Nissemanen@users.noreply.github.com>
2025-04-23 12:37:57 +0200

    Update blocks.py

--- src/tabby/blocks.py
+++ src/tabby/blocks.py
@@ -1,162 +1,162 @@
 class MotionBlocks:
     def move(self, steps: float):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def turn(self, degrees: float): # clockwise
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def go_to(self, position):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def glide_to(self, seconds: float, position):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def point_in_direction(self, degrees: int): #degrees
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def point_towards(self, thing): # position
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def change_x_by(self, amount: int):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def set_x_to(self, amount: int):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def change_y_by(self, amount: int):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def set_y_to(self, amount: int):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def if_on_edge_bounce(self):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def set_rotation_style(self, style):
-        pass
+        raise NotImplementedError("This method is not available.")
 
 
 class LooksBlocks:
     def say(self, text: str, seconds: float = None):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def think(self, text: str, seconds: float = None):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def switch_costume_to(self, costume):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def next_costume(self):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def switch_backdrop_to(self, backdrop: str, wait: bool = False):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def next_backdrop(self):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def change_size_by(self, amount: int):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def set_size_to(self, amount: int):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def change_effect_by(self, effect, amount: float):
         if effect not in ["color", "fisheye", "whirl", "pixelate", "mosaic", "brightness", "ghost"]:
             raise ValueError(f'Invalid value for "effect". Expected "color", "fisheye", "whirl", "pixelate", "mosaic", "brightness" or "ghost"')
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def set_effect_to(self, effect, amount: float):
         if effect not in ["color", "fisheye", "whirl", "pixelate", "mosaic", "brightness", "ghost"]:
             raise ValueError(
                 f'Invalid value for "effect". Expected "color", "fisheye", "whirl", "pixelate", "mosaic", "brightness" or "ghost"')
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def clear_graphic_effects(self):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def show(self):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def hide(self):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def go_to_layer(self, layer, amount: int = None):
-        pass
+        raise NotImplementedError("This method is not available.")
 
 class SoundBlocks:
     def play_sound(self, sound, until_done: bool = False):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def stop_all_sounds(self):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def change_effect_by(self, effect, amount: float):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def set_effect_to(self, effect, amount: float):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def clear_all_effects(self):
-        pass
+        raise NotImplementedError("This method is not available.")
         
     def change_volume(self):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def set_volume(self):
-        pass
+        raise NotImplementedError("This method is not available.")
 
 class EventBlocks:
     def when_green_flag_clicked(self, func):
         def wrapper():
-            pass
+            raise NotImplementedError("This method is not available.")
 
     def when_key_pressed(self, func, key):
         def wrapper():
-            pass
+            raise NotImplementedError("This method is not available.")
 
     def when_this_sprite_clicked(self, func):
         def wrapper():
-            pass
+            raise NotImplementedError("This method is not available.")
 
     def when_backdrop_switches_to(self, func, backdrop):
         def wrapper():
-            pass
+            raise NotImplementedError("This method is not available.")
 
     def when_x_is_bigger_then_y(self, func, first, second):
         def wrapper():
-            pass
+            raise NotImplementedError("This method is not available.")
 
     def when_i_recive(self, func, message):
         def wrapper():
-            pass
+            raise NotImplementedError("This method is not available.")
 
     def broudcast(self, message, wait: bool = False):
-        pass
+        raise NotImplementedError("This method is not available.")
 
 class ControlBlocks:
     #finnish when i know what to do
-    pass
+    raise NotImplementedError("This method is not available.")
     
 
 # Backdrop
 class BackdropLooksBlocks:
     def switch_backdrop_to(self, backdrop: str, wait: bool = False):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def next_backdrop(self):
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def change_effect_by(self, effect, amount: float):
         if effect not in ["color", "fisheye", "whirl", "pixelate", "mosaic", "brightness", "ghost"]:
             raise ValueError(f'Invalid value for "effect". Expected "color", "fisheye", "whirl", "pixelate", "mosaic", "brightness" or "ghost"')
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def set_effect_to(self, effect, amount: float):
         if effect not in ["color", "fisheye", "whirl", "pixelate", "mosaic", "brightness", "ghost"]:
             raise ValueError(
                 f'Invalid value for "effect". Expected "color", "fisheye", "whirl", "pixelate", "mosaic", "brightness" or "ghost"')
-        pass
+        raise NotImplementedError("This method is not available.")
 
     def clear_graphic_effects(self):
-        pass
+        raise NotImplementedError("This method is not available.")
