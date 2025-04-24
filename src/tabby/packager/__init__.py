47c9950369faf5e6f7595ae515b3291ac24bb892
Nissemanen <99994541+Nissemanen@users.noreply.github.com>
2025-04-24 11:49:57 +0200

    Update __init__.py

--- src/tabby/packager/__init__.py
+++ src/tabby/packager/__init__.py
@@ -1,6 +1,9 @@
+import os
 from .package import package
 
+__all__ = []
 
+final_product_dir = os.path.join(os.path.dirname(__file), "_final_produt.json")
 
 with open("._final_product.json", "w") as _f:
     _f.write("")
